9ecea70050bdb62cc15014e29346a58f3d5726d5
Shivansh Bhardwaj <96634814+bhardwajshivansh@users.noreply.github.com>
2023-04-05 16:05:25 +0530

    installed necessory modules

--- client/src/pages/BookingPage.js
+++ client/src/pages/BookingPage.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout";
-import axios from "axios";
 import { useParams } from "react-router-dom";
-import { DatePicker, TimePicker } from "antd";
-import moment from "antd";
+import axios from "axios";
+import { DatePicker, message, TimePicker } from "antd";
+import moment from "moment";
+import { useDispatch, useSelector } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 
 const BookingPage = () => {
+  const { user } = useSelector((state) => state.user);
   const params = useParams();
   const [doctors, setDoctors] = useState([]);
-  const [data, setDate] = useState();
-  const [timings, setTimings] = useState();
-  const [isAvailable, setIsAVAILABLE] = useState();
-  //login user data
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [isAvailable, setIsAvailable] = useState();
+  const dispatch = useDispatch();
+  // login user data
   const getUserData = async () => {
     try {
       const res = await axios.post(
-        "/api/vi/doctor/getDoctorById",
+        "/api/v1/doctor/getDoctorById",
         { doctorId: params.doctorId },
         {
           headers: {
@@ -30,6 +34,36 @@ const BookingPage = () => {
       console.log(error);
     }
   };
+  // =============== booking func
+  const handleBooking = async () => {
+    try {
+      dispatch(showLoading());
+      const res = await axios.post(
+        "/api/v1/user/book-appointment",
+        {
+          doctorId: params.doctorId,
+          userId: user._id,
+          doctorInfo: doctors,
+          userInfo: user,
+          date: date,
+          time: time,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (res.data.success) {
+        message.success(res.data.message);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getUserData();
     //eslint-disable-next-line
@@ -41,32 +75,33 @@ const BookingPage = () => {
         {doctors && (
           <div>
             <h4>
-              Dr.{doctors.firstname}
-              {doctors.lastname}
+              Dr.{doctors.firstName} {doctors.lastName}
             </h4>
-            <h5>Fees:{doctors.feesperCunsaltation}</h5>
-            <h5>
-              Timings :{doctors.timings[0]}-{doctors.timings[1]}
-            </h5>
+            <h4>Fees : {doctors.feesPerCunsaltation}</h4>
+            <h4>
+              Timings : {doctors.timings && doctors.timings[0]} -{" "}
+              {doctors.timings && doctors.timings[1]}{" "}
+            </h4>
             <div className="d-flex flex-column w-50">
-              <DatePicker 
-              className="m-2"
-                format="DD-MM-YY"
+              <DatePicker
+                className="m-2"
+                format="DD-MM-YYYY"
                 onChange={(value) =>
                   setDate(moment(value).format("DD-MM-YYYY"))
                 }
               />
-              <TimePicker.RangePicker
+              <TimePicker
                 format="HH:mm"
-                onChange={(values) =>
-                  setTimings([
-                    moment(values[0]).format("HH:mm"),
-                    moment(values[1]).format("HH:mm"),
-                  ])
-                }
+                className="m-2"
+                onChange={(value) => {
+                  setTime(moment(value).format("HH:mm"));
+                }}
               />
-              <button className="btn-btn-primary mt-2">
+              <button className="btn btn-primary mt-2">
                 Check Availability
+              </button>
+              <button className="btn btn-dark mt-2" onClick={handleBooking}>
+                Book Now
               </button>
             </div>
           </div>
@@ -75,4 +110,5 @@ const BookingPage = () => {
     </Layout>
   );
 };
+
 export default BookingPage;
