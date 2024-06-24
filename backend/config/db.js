934ab406dfcd2e93ebdff45be9b88a01e9941e09
bhardwajshivansh <96634814+bhardwajshivansh@users.noreply.github.com>
2024-06-24 18:19:08 +0530

    Update db.js
    updated config

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const colors=require('colors')//this is for color in terminal
 
 const connectDB = async( )  => {
     try{
-await mongoose.connect(process.env.MONGODB_URL)
-console.log('Mongodb connected ${mongoose.connection.host}' .bgGreen.white);
+         await mongoose.connect(process.env.MONGODB_URL)
+         console.log('Mongodb connected ${mongoose.connection.host}' .bgGreen.white);
     }catch(error){
         console.log('mongodb Server Issue ${error}'.bgRed.white);
     }
